02c9827b413db2170ff35cadd1ccfbb66245c4e0
gudals7856 <gudals7856@gmail.com>
2021-08-14 22:22:58 +0900

    배달

--- delivery/src/Solution.java
+++ delivery/src/Solution.java
@@ -1,52 +1,78 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.PriorityQueue;
 
 class Solution {
 
     public static void main(String[] args) {
-        int[][] road1 = {{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
-        int[][] road2 = {{1,2,1},{1,3,2},{2,3,2},{3,4,3},{3,5,2},{3,5,3},{5,6,1}};
+        int[][] road1 = {{1, 2, 1}, {2, 3, 3}, {5, 2, 2}, {1, 4, 2}, {5, 3, 1}, {5, 4, 2}};
+        int[][] road2 = {{1, 2, 1}, {1, 3, 2}, {2, 3, 2}, {3, 4, 3}, {3, 5, 2}, {3, 5, 3}, {5, 6, 1}};
+
         System.out.println(solution(5, road1, 3));
         System.out.println(solution(6, road2, 4));
     }
 
-    static HashSet<Integer> check = new HashSet<>();
+    static class Edge implements Comparable<Edge> {
+        int to;
+        int cost;
+
+        public Edge(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    static PriorityQueue<Edge> pq;
+    static ArrayList<ArrayList<Edge>> edges;
+    static int[] dist;
 
     public static int solution(int N, int[][] road, int K) {
         int answer = 0;
 
+        pq = new PriorityQueue<>();
+        edges = new ArrayList<>();
+        dist = new int[N + 1];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        for (int i = 0; i < N + 1; i++) edges.add(new ArrayList<>());
+
         for (int i = 0; i < road.length; i++) {
-            if (road[i][0] == 1 && road[i][2] <= K) {
-                ArrayList<Integer> route = new ArrayList<>();
-                int sum = road[i][2];
+            int from = road[i][0];
+            int to = road[i][1];
+            int weight = road[i][2];
 
-                check.add(1);
-                route.add(1);
-
-                nextTown(road, road[i][1], route, K, sum);
-            }
+            edges.get(from).add(new Edge(to, weight));
+            edges.get(to).add(new Edge(from, weight));
         }
 
-        answer = check.size();
+        dist[1] = 0;
+        pq.offer(new Edge(1, 0));
+
+        dijkstra();
+
+        for (int i = 0; i < dist.length; i++) {
+            if (dist[i] <= K) answer++;
+        }
+
         return answer;
+
     }
 
-    public static void nextTown(int[][] road, int now, ArrayList<Integer> route, int K, int sum) {
+    private static void dijkstra() {
+        while (!pq.isEmpty()) {
+            Edge e = pq.poll();
 
-        for (int i = 0; i < road.length; i++) {
-            if (road[i][0] == now && road[i][2] <= K) {
-
-                // 이미 지나온 길이라면 확인하지 않음.
-                if (route.contains(road[i][1])) continue;
-
-                sum += road[i][2];
-                if (sum > K) return;
-
-                check.add(now);
-                route.add(now);
-
-                nextTown(road, road[i][1], route, K, sum);
+            for (int i = 0; i < edges.get(e.to).size(); i++) {
+                Edge edge = edges.get(e.to).get(i);
+                if (dist[edge.to] > dist[e.to] + edge.cost) {
+                    dist[edge.to] = dist[e.to] + edge.cost;
+                    pq.offer(edge);
+                }
             }
         }
     }
